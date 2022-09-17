73d38640ec59167d960d333110f7fe39354b7d28
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2022-09-17 14:05:04 +0530

    done

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+//const { response } = require("express");
+
 var btn = document.querySelector("#btn");
-var txtinput= document.querySelector("#text-input");
+var txtinput = document.querySelector("#text-input");
 
-var outputDiv=document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-
-console.log(txtinput);
-
-function clickHandler(){ 
-    outputDiv.innerText="djfjnjvfj" + txtinput.value;
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + txtinput;
 }
 
-btn.addEventListener("click",clickHandler)
+//console.log(txtinput);
+
+function errorHandler(error) {
+    console.log("Error Occured ", error);
+    alert("Something Wrong with server!!Try Again after some time")
+}
+function clickHandler() {
+    var inputtxt = txtinput.value;
+    fetch(getTranslationURL(inputtxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        output.innerText=translatedText;
+    })
+    .catch(errorHandler)
+}
+
+btn.addEventListener("click", clickHandler);
